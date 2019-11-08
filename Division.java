a8d0ca6a739a2544bf466287954e59858b55a15a
mdlks <m_dlks@outlook.com>
2019-11-08 21:48:11 +0100

    ejercicio 1 resuelto

---
+++ Division.java
@@ -0,0 +1,13 @@
+package practicasJava1;
+
+public class Division {
+
+	public static void main(String[] args) {
+		
+		double numero1=1234;
+		double numero2=532;
+		double resultado=numero1/numero2;
+		System.out.printf("el valor de la division es %015.2f",resultado);
+	}
+
+}
